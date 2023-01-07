da5a1172ea3810c70b99eda73d8cf6059b57a450
Zinaida Erkuru <zikonnikova@gmail,com>
2023-01-07 15:46:44 +0100

    users app

---
+++ TODO/users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.EmailField(primary_key=True)
