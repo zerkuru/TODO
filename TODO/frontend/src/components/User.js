168af075602a439dbc1eb2a10f8205c6feb65321
Zinaida Erkuru <zikonnikova@gmail,com>
2023-01-21 16:40:06 +0100

    front+back

---
+++ TODO/frontend/src/components/User.js
@@ -0,0 +1,40 @@
+import React from 'react'
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.username}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+return (
+    <table>
+        <th>
+            First name
+        </th>
+        <th>
+            Last Name
+        </th>
+        <th>
+            Username
+        </th>
+        <th>
+            Email
+        </th>
+        {users.map((user) => <UserItem user={user} />)}
+    </table>
+)
+}
+export default UserList
