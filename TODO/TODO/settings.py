7efb115c7e45ec804323134969a5e3003b00f867
Zinaida Erkuru <zikonnikova@gmail,com>
2023-01-21 18:27:32 +0100

    new models

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -130,3 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+REST_FRAMEWORK = {
+     'DEFAULT_RENDERER_CLASSES': [
+         'rest_framework.renderers.JSONRenderer',
+         'rest_framework.renderers.BrowsableAPIRenderer',
+     ]
+}
--- TODO/users/models.py
+++ TODO/users/models.py
@@ -5,3 +5,17 @@ class User(models.Model):
     firstname = models.CharField(max_length=64)
     lastname = models.CharField(max_length=64)
     email = models.EmailField(primary_key=True)
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=64)
+    reference = models.CharField(max_length=300)
+    members = models.ManyToManyField(User)
+
+
+class ToDo(models.Model):
+    project = models.ForeignKey(Project, null=True, on_delete=models.SET_NULL)
+    text = models.CharField(max_length=300)
+    creator = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    creation_date = models.DateField()
+    update = models.DateField()
