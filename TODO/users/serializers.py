6987959743e8d7c919375a3aa4f405ef07618e54
Zinaida Erkuru <zikonnikova@gmail,com>
2023-01-21 19:51:00 +0100

    finish3

--- TODO/users/serializers.py
+++ TODO/users/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import User
+from .models import User, ToDo, Project
 
 
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class ProjectModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+class ToDoModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = '__all__'
+
+
